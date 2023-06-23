d817ae1c2aa28c7916dd8ccb56dfb1234c34c05d
JoseCordova2 <127773472+JonathanMora69@users.noreply.github.com>
2023-06-23 09:29:58 -0500

    Agregar archivos de proyecto.

---
+++ CRUDEmpleado/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CRUDEmpleado.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDEmpleado.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            //Aquí va la cadena de conexión
+            optionsBuilder.UseMySQL("server=localhost; database=23BM; user=root; password=1234");
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
